e030c20c03e834e29e809f4f8822fc685b8d8073
IngridReif <ingrid.reif@gmail.com>
2022-08-29 16:56:02 -0300

    Arrumando identação

--- 2-questaoDois.js
+++ 2-questaoDois.js
@@ -2,11 +2,11 @@
 //O === além de comparar o resultado, irá identificar o tipo.
 //Ex:
 
-    var a = "1";
-    var b = 1;
+var a = "1";
+var b = 1;
 
-    console.log (a == b);
-    console.log (a === b);
+console.log (a == b);
+console.log (a === b);
 
 // No "==" ele apenas comparou os valores, então o resultado será "true", mesmo um sendo string e o outro número.
 //No "===" ele comparou o tipo e o valor, imprimindo "false", pois os dois não são iguais.
