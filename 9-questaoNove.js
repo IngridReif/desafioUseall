f191e0713f47715bd23a5bb633b3a2eae4f5379b
IngridReif <ingrid.reif@gmail.com>
2022-08-29 10:27:12 -0300

    Resultado da questão número 9

---
+++ 9-questaoNove.js
@@ -0,0 +1,15 @@
+var a = 3;
+var b = 5;
+
+//Toda vez que A for igual a A, ela ficará igual 5.
+if (a === a) {
+    a = 5;
+}
+
+//Toda vez que B for igual a B, ela ficará igual a 3.
+if (b === b) {
+    b = 3;
+}
+
+console.log ("Variável a alterada: " + a);
+console.log ("Variável b alterada: " + b);
