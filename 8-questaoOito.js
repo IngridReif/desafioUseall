73634561b2acb579220802b2775e98e115cbc4c4
IngridReif <ingrid.reif@gmail.com>
2022-08-29 16:57:09 -0300

    Organizando comentários

--- 8-questaoOito.js
+++ 8-questaoOito.js
@@ -1,16 +1,19 @@
+//A função é feita para que não seja necessário repetir o código toda vez que ele precisar percorrer uma nova lista.
 function calculaMedia(lista) {
-    var valorTotal = 0; //O valor total se iniciará em zero
-
+    //O valor total se iniciará em zero
+    var valorTotal = 0; 
+    
+    /*Inicialmente tanto o i, quanto o valorTotal iniciarão em zero, conforme a lista é percorrida, os valores são
+    substituídos e somados*/
     for (i=0; i < lista.length; i++) {
         valorTotal += lista[i];
-    } /*Inicialmente tanto o i, quanto o valorTotal iniciarão em zero, conforme a lista é percorrida, os valores são
-    substituídos e somados*/
-
-    divisao = valorTotal/lista.length; /*Na divisão, o valorTotal será dividido pelo tamanho da lista*/ 
+    } 
+    
+    /*Na divisão, o valorTotal será dividido pelo tamanho da lista*/ 
+    divisao = valorTotal/lista.length; 
 
     console.log(divisao);
 }
-//A função é feita para que não seja necessário repetir o código toda vez que ele precisar percorrer uma nova lista.
 
 // Função com três valores
 var lista = [2, 4, 6];
