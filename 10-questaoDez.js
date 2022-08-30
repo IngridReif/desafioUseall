b36f932b7d2dc10a38bfdf9b7809858c28f0cb68
IngridReif <ingrid.reif@gmail.com>
2022-08-30 14:38:59 -0300

    Comentando questão número 10

--- 10-questaoDez.js
+++ 10-questaoDez.js
@@ -1,14 +1,21 @@
+//A função foi feita para verificar se existem ou não números repetidos 
 function verificaNumerosRepetidos(listaNumeros) {
+    //A lista de números repetidos se inicia vazia
     var listaRepetidos = [];
-
+    /*O primeiro "for" irá passar por toda a lista de números, já o segundo, irá passar pela mesma lista, adicionando +1 
+    a sua contagem de posição, assim todos os números da lista poderão ser verificados.*/
     for(i=0; i < listaNumeros.length; i++) {
         for(x=i+1; x < listaNumeros.length; x++) {
+            /*O "if" estará fazendo a comparação entre os números passados pelo "for", então se existir um número igual
+            entre elas, ele será adicionado na listaRepetidos. */
             if(listaNumeros[i] == listaNumeros[x]) {
                 listaRepetidos.push(listaNumeros[i]);
             }
         }
     }
     
+    /*Então, se não existirem números repetidos, o programa avisára, mas se existirem números repetidos, eles serão impressos
+    em uma nova lista, usando join para separá-los. */
     if (listaRepetidos.length === 0) {
         console.log("Não há números repetidos");
     } else {
