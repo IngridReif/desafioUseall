0cf7e501bfd88f53fd0bdf614b6f319330fa3367
IngridReif <ingrid.reif@gmail.com>
2022-09-02 13:13:34 -0300

    Melhornado resposta questão 4

--- 4-questaoQuatro.js
+++ 4-questaoQuatro.js
@@ -10,5 +10,7 @@ var novoNumero = minhaFuncao (a);
 console.log("novoNumero = " + novoNumero);
 console.log("a = " + a);
 
-//No primeiro console.log, o número que irá aparecer é o "9", já que na função o número dado a variável minhaFuncao é "9"
+/*No primeiro console.log, o número que irá aparecer é o "9", já que a variável minha função é uma função, que recebe 
+um número qualquer como parâmetro, porém, o o parâmetro número sempre é atribuido "9", retornando o mesmo, independente 
+do valor atribuido ao parâmetro.*/
 //No segundo console.log, o número que irá aparecer é o "3", por ser o número indicado na variável "a"
